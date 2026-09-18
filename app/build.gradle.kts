91d73574e2372246991e0dff15715984ea831fe5
Aman Pandey <amanpandey9299@gmail.com>
2026-09-18 08:27:51 +0530

    Configure Android instrumentation tests

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -27,6 +27,7 @@ android {
         targetSdk = 36
         versionCode = 1
         versionName = "0.1.0"
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
     buildFeatures { compose = true }
