9936da6e69c1e8d0f4a1fd69c93aa405e10cff36
Aman Pandey <amanpandey9299@gmail.com>
2026-09-18 09:51:09 +0530

    Fix terminal session binding import

--- app/src/main/java/com/mrredhood/devforge/core/terminal/TerminalViewModel.kt
+++ app/src/main/java/com/mrredhood/devforge/core/terminal/TerminalViewModel.kt
@@ -17,6 +17,7 @@ import kotlinx.coroutines.Job
 import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.flow.collectLatest
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 import org.json.JSONArray
 import org.json.JSONObject
 
