483589fee338d6c9c7142d025fd3f178e8964b47
Aman Pandey <amanpandey9299@gmail.com>
2026-09-25 05:00:00 +0530

    Fix live preview Kotlin escaping

--- app/src/main/java/com/mrredhood/devforge/core/preview/PreviewScreen.kt
+++ app/src/main/java/com/mrredhood/devforge/core/preview/PreviewScreen.kt
@@ -201,7 +201,7 @@ private object PreviewDocumentRenderer {
         value.replace("&", "&amp;")
             .replace("<", "&lt;")
             .replace(">", "&gt;")
-            .replace(""", "&quot;")
+            .replace("\"", "&quot;")
             .replace("'", "&#39;")
 
     private fun escapeScript(value: String): String =
