f9cd8e8f52a8b5a9a77fd05319b1f60e2372e542
Aman Pandey <amanpandey9299@gmail.com>
2026-09-25 10:53:33 +0530

    Fix Live Preview Alignment import

--- app/src/main/java/com/mrredhood/devforge/core/preview/PreviewScreen.kt
+++ app/src/main/java/com/mrredhood/devforge/core/preview/PreviewScreen.kt
@@ -7,7 +7,6 @@ import android.webkit.WebView
 import android.webkit.WebViewClient
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Alignment
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -28,6 +27,7 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.viewinterop.AndroidView
